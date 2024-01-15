9657e086eb603b2e65455a736e634b778bdbd6c4
pointmeathesky <146136741+pointmeathesky@users.noreply.github.com>
2024-01-14 18:29:31 -0800

    Update app.py

--- Week1/app.py
+++ Week1/app.py
@@ -1,13 +1,21 @@
 from flask import Flask,render_template,request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route('/from')
+@app.route('/')
+
+def homepage():
+
+    return render_template('index.html')
+
+
+@app.route('/form', methods=['POST'])
+
 def form():
-    return render_template('form.html')
+    data = request.form['form']
+    # message = "You searched for: " + data
+    # return {"You searched for: " :data}
+    return render_template('search.html', data=data)
 
-@app.route('/data/', methods = ['POST'])
-def data():
-    return render_template('data.html', form_data = form_data)
-
-app.run(host='localhost', port=2500)
+if __name__ == "main":
+    app.run(host='localhost', port=5000)
